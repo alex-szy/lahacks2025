f84b2252368d6111d6f5375913c5421fc11d39e6
g1oom <albertowj2001@gmail.com>
2025-04-26 22:12:31 -0700

    Changes to db save and store functionalities

--- db/database.py
+++ db/database.py
@@ -1,62 +1,56 @@
-# from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.operations import SearchIndexModel
-# import os
 import time
 
 class VectorDatabase:
-    def __init__(self, connection_string, db_name, collection_name):
-        self.client = MongoClient(connection_string)
-        self.db_name = db_name
-        self.collection_name = collection_name
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        self.db_name = "file_system"
+        self.collection_name = "embedded_file"
 
+        client = MongoClient(self.connection_string)
+        if self.collection_name not in client[self.db_name].list_collection_names():
+            print("Creating collection...")
+            client[self.db_name].create_collection(self.collection_name)
+        client.close()
+
+    """
+    Stores embedding vector to db
+    Input:
+        embedding_vector: List
+        file_path: str
+    Output:
+        None
+    """
+    # embedding vector: list
+    # file_path: string
     def store_embedding_vector(self, embedding_vector, file_path):
-        self.client[self.db_name][self.collection_name].insert_one({
+        client = MongoClient(self.connection_string)
+        client[self.db_name][self.collection_name].insert_one({
             "file_path": file_path,
-            "file_embedding": embedding_vector.tolist()
+            "file_embedding": embedding_vector
         })
-
-    def save_search_index(self):
-        search_index_model = SearchIndexModel(
-            definition={
-                "fields": [
-                    {
-                        "type": "vector",
-                        "path": "file_embedding",
-                        "numDimensions": 1536,
-                        "similarity": "cosine",
-                        "quantization": "scalar"
-                    }
-                ]
-            },
-            name="vector_index",
-            type="vectorSearch"
-        )
-
-        result = self.client[self.db_name][self.collection_name].create_search_index(model=search_index_model)
-        print("New search index named " + result + " is building.")
-
-        print("Polling to check if the index is ready. This may take up to a minute.")
-        predicate=None
-        if predicate is None:
-            predicate = lambda index: index.get("queryable") is True
-
-        while True:
-            indices = list(self.client[self.db_name][self.collection_name].list_search_indexes(result))
-            if len(indices) and predicate(indices[0]):
-                break
-            time.sleep(5)
-        print(result + " is ready for querying.")
-
-    def get_query_results(self, query_embedding):
+        client.close()
+    
+    """
+    Performs cosine search and get closest query
+    Input:
+        query_embedding: List
+        num_candidates: int
+        limit: int
+    Output:
+        result: List[Dict] (descending order of similarity score, dict contains "file_path" key)
+    """
+    def get_query_results(self, query_embedding, num_candidates, limit):
+        client = MongoClient(self.connection_string)
         pipeline = [
             {
                 '$vectorSearch': {
-                    'index': 'vector_index', 
+                    'index': "vector_index", 
                     'path': "file_embedding", 
                     'queryVector': query_embedding, 
-                    'numCandidates': 75, 
-                    'limit': 5
+                    'numCandidates': num_candidates, 
+                    'limit': limit
                 }
             },
             {
@@ -71,8 +65,43 @@ class VectorDatabase:
         ]
 
         # run pipeline
-        result = self.client[self.db_name][self.collection_name].aggregate(pipeline)
+        result = client[self.db_name][self.collection_name].aggregate(pipeline)
+        client.close()
         return result
+
+    def save_search_index(self, num_dimensions):
+        client = MongoClient(self.connection_string)
+        search_index_model = SearchIndexModel(
+            definition={
+                "fields": [
+                    {
+                        "type": "vector",
+                        "path": "file_embedding",
+                        "numDimensions": num_dimensions,
+                        "similarity": "cosine",
+                        "quantization": "scalar"
+                    }
+                ]
+            },
+            name="vector_index",
+            type="vectorSearch"
+        )
+
+        result = client[self.db_name][self.collection_name].create_search_index(model=search_index_model)
+        print("New search index named " + result + " is building.")
+
+        print("Polling to check if the index is ready. This may take up to a minute.")
+        predicate=None
+        if predicate is None:
+            predicate = lambda index: index.get("queryable") is True
+
+        while True:
+            indices = list(client[self.db_name][self.collection_name].list_search_indexes(result))
+            if len(indices) and predicate(indices[0]):
+                break
+            time.sleep(5)
+        print(result + " is ready for querying.")
+        client.close()
 
 # def main():
 #     try:

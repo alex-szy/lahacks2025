266984559cb2cf2b3283f126e3cdf8f73220d402
Alex Su <suziyue@gmail.com>
2025-04-27 04:40:55 -0700

    raw versions of assoc commands

--- commands/assoc.py
+++ commands/assoc.py
@@ -8,27 +8,18 @@ def assoc():
     pass
 
 
-@assoc.command()
-@click.argument('folder_path')
-@click.option('--description', prompt="Description of the folder", help="Associate this folder with a certain kind of files.")
-def add(folder_path, description):
-    """Add a folder association."""
+def _add(folder_path, description):
     cfg = FileSystemConfig()
     cfg.append_entry(folder_path, description)
     click.echo(f"Sucessfully added: {folder_path}, {description}")
 
 
-@assoc.command()
-@click.argument('folder_path')
-def remove(folder_path):
-    """Remove a folder association."""
+def _remove(folder_path):
     cfg = FileSystemConfig()
     cfg.remove_entry(folder_path)
 
 
-@assoc.command(name="list")
-def list_assoc():
-    """List folder associations."""
+def _list():
     cfg = FileSystemConfig()
     path_list = cfg.read_all_entries()
 
@@ -37,3 +28,24 @@ def list_assoc():
         click.echo(path)
         click.echo(info)
         click.echo("")
+
+
+@assoc.command()
+@click.argument('folder_path')
+@click.option('--description', prompt="Description of the folder", help="Associate this folder with a certain kind of files.")
+def add(folder_path, description):
+    """Add a folder association."""
+    _add(folder_path, description)
+
+
+@assoc.command()
+@click.argument('folder_path')
+def remove(folder_path):
+    """Remove a folder association."""
+    _remove(folder_path)
+
+
+@assoc.command(name="list")
+def list_assoc():
+    """List folder associations."""
+    _list()

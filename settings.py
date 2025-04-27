a68654e9b4f784e2f2db1d2f84964f88d7d115d3
Alex Su <suziyue@gmail.com>
2025-04-27 06:13:29 -0700

    Fixed file not found

--- settings.py
+++ settings.py
@@ -6,18 +6,19 @@ BASE_DIR = Path(__file__).resolve().parent
 CONFIG_DIR = BASE_DIR / "config"
 ASSETS_DIR = BASE_DIR / "assets"
 
-if not CONFIG_DIR.exists():
-    raise Exception(f"Config folder not found: {CONFIG_DIR}")
+os.makedirs(CONFIG_DIR, exist_ok=True)
 
 # Guaranteed to exist, else exception raised
 WATCH_PATHS_FILE = CONFIG_DIR / "watch_paths.json"
 if not WATCH_PATHS_FILE.exists():
-    raise Exception(f"Watch paths file not found: {WATCH_PATHS_FILE}")
+    with open(WATCH_PATHS_FILE, "w") as f:
+        f.write("[]")
 
 # Guaranteed to exist, else exception raised
 FOLDER_PATHS_FILE = CONFIG_DIR / "folder_paths.json"
 if not FOLDER_PATHS_FILE.exists():
-    raise Exception(f"Folder paths file not found: {FOLDER_PATHS_FILE}")
+    with open(FOLDER_PATHS_FILE, "w") as f:
+        f.write("[]")
 
 DOTENV_PATH = BASE_DIR / ".env"
 

9f5b24b298d70ca559fbc3ca273bdfe3a8a5a357
Arsh D <arshnoorfly@ucla.edu>
2025-04-27 00:21:11 -0700

    moved PATH resolver stuff to settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent
 CONFIG_DIR = BASE_DIR / "config"
@@ -15,3 +17,15 @@ if not WATCH_PATHS_FILE.exists():
 FOLDER_PATHS_FILE = CONFIG_DIR / "folder_paths.json"
 if not FOLDER_PATHS_FILE.exists():
     raise Exception(f"Folder paths file not found: {FOLDER_PATHS_FILE}")
+
+DOTENV_PATH = BASE_DIR / ".env"
+
+if not DOTENV_PATH.exists():
+    raise FileNotFoundError(f"Missing .env file at {DOTENV_PATH}")
+
+load_dotenv(dotenv_path=DOTENV_PATH)
+
+# Guaranteed to exist, else exception raised
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY is not set in .env")

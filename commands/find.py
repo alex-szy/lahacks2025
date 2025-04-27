0d52364e41aec2a172121393815bc6ca3e4639eb
Alex Su <suziyue@gmail.com>
2025-04-27 06:33:04 -0700

    Reverted find.py

--- commands/find.py
+++ commands/find.py
@@ -3,6 +3,7 @@ import os
 from settings import MONGO_URI
 from typing import List
 from models.file import File
+import sys
 import contextlib
 
 
@@ -13,8 +14,9 @@ def _find(query: str) -> List[File]:
     db = VectorDatabase(MONGO_URI)
     qp = QueryProcessor(Encoder(), db=db)
 
-    res = qp.process_query(query)
-    
+    with contextlib.redirect_stderr(open(os.devnull, "w")):
+        res = qp.process_query(query)
+
     return res
 
 
@@ -22,14 +24,7 @@ def _find(query: str) -> List[File]:
 @click.argument('query')
 def find(query: str):
     """Semantic search for files using a QUERY string and return file paths."""
-    from db.database import VectorDatabase
-    from engine.encoder import Encoder
-    from engine.queryprocessor import QueryProcessor
-    db = VectorDatabase(MONGO_URI)
-    qp = QueryProcessor(Encoder(), db=db)
-
-    with contextlib.redirect_stderr(open(os.devnull, "w")):
-        res = qp.process_query(query)
+    res = _find(query)
 
     for r in res:
-        click.echo(r)
+        click.echo(f"{r.path}: {r.summary}")
